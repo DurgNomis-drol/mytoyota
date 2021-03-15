085be8400f83bb3d5ba178565c4125324082c3f5
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2021-03-15 14:24:20 +0100

    added-UUID-and-TOKEN-validation (#9)

--- mytoyota/utils.py
+++ mytoyota/utils.py
@@ -1,7 +1,27 @@
 """Toyota Connected Services API."""
+from uuid import UUID
 from langcodes import Language
 
+from .exceptions import ToyotaInvalidToken
 
-def locale_is_valid(locale: str) -> bool:
+
+def is_valid_locale(locale: str) -> bool:
     """Is locale string valid."""
     return Language.make(locale).is_valid()
+
+
+def is_valid_uuid(uuid_to_test, version=4):
+    """Checks if uuid string is valid"""
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == uuid_to_test
+
+
+def is_valid_token(token):
+    """Checks if token is the correct length"""
+    if len(token) == 114:
+        return True
+
+    raise ToyotaInvalidToken("Token length must be 114 characters long.")
