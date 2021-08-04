cebc742b4373231c3f1c1da1834e0595176053fa
Simon Hansen <67142049+DurgNomis-drol@users.noreply.github.com>
2021-08-04 17:42:14 +0200

    Remove uuid dependency (#35)

--- mytoyota/utils.py
+++ mytoyota/utils.py
@@ -1,5 +1,4 @@
 """Toyota Connected Services API."""
-from uuid import UUID
 from langcodes import Language
 
 from .const import TOKEN_LENGTH
@@ -9,15 +8,6 @@ from .exceptions import ToyotaInvalidToken
 def is_valid_locale(locale: str) -> bool:
     """Is locale string valid."""
     return Language.make(locale).is_valid()
-
-
-def is_valid_uuid(uuid_to_test, version=4):
-    """Checks if uuid string is valid"""
-    try:
-        uuid_obj = UUID(uuid_to_test, version=version)
-    except ValueError:
-        return False
-    return str(uuid_obj) == uuid_to_test
 
 
 def is_valid_token(token):
