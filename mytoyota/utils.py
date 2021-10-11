75d544795a8ff76219a53a2b5b257ad5e1deca88
John de Rooij <joro75@users.noreply.github.com>
2021-10-11 19:25:20 +0200

    Correctly handle a possible divide by zero in convert_to_mpg (#78)

--- mytoyota/utils.py
+++ mytoyota/utils.py
@@ -55,4 +55,6 @@ def convert_to_liter_per_100_miles(liters: float) -> float:
 def convert_to_mpg(liters_per_100_km: float) -> float:
     """Convert to miles per UK gallon (MPG)"""
     _LOGGER.debug("Converting to MPG...")
-    return round(282.5 / liters_per_100_km, 4)
+    if liters_per_100_km > 0.0:
+        return round(282.5 / liters_per_100_km, 4)
+    return 0.0
