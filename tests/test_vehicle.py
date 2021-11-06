a3c09de5b570bd326fe562757ad10da6a2c0a4f3
DurgNomis-drol <simongrud@gmail.com>
2021-11-06 10:18:37 +0100

    Bugfix

---
+++ tests/test_vehicle.py
@@ -0,0 +1,77 @@
+"""pytest tests for mytoyota.vehicle.Vehicle"""
+import json
+import os
+
+from mytoyota.vehicle import Vehicle
+
+# pylint: disable=no-self-use
+
+
+class TestVehicle:
+    """pytest functions for Vehicle object"""
+
+    @staticmethod
+    def _load_from_file(filename: str):
+        """Load a data structure from the specified JSON filename, and
+        return it."""
+        with open(filename, encoding="UTF-8") as json_file:
+            return json.load(json_file)
+
+    def test_vehicle_no_data(self):
+        """Test vehicle with no initialization data"""
+        vehicle = Vehicle({})
+
+        assert vehicle.id == 0
+        assert vehicle.vin is None
+        assert vehicle.alias is None
+        assert vehicle.details is None
+        assert vehicle.is_connected is False
+        assert vehicle.odometer is None
+        assert vehicle.energy is None
+        assert vehicle.hvac is None
+        assert vehicle.parking is None
+
+    def test_vehicle_init_no_status(self):
+        """Test vehicle initialization with no status"""
+
+        data_files = os.path.join(os.path.curdir, "tests", "data")
+
+        fixtures = self._load_from_file(os.path.join(data_files, "vehicles.json"))
+
+        for veh in fixtures:
+            vehicle = Vehicle(vehicle_info=veh)
+
+            assert vehicle.vin == veh.get("vin")
+            assert vehicle.is_connected is False
+            assert vehicle.odometer is None
+            assert vehicle.energy is None
+            assert vehicle.hvac is None
+            assert vehicle.parking is None
+
+    def test_vehicle_init(self):
+        """Test vehicle initialization with connected services"""
+
+        data_files = os.path.join(os.path.curdir, "tests", "data")
+
+        fixtures = self._load_from_file(os.path.join(data_files, "vehicles.json"))
+
+        for veh in fixtures:
+            vehicle = Vehicle(
+                vehicle_info=veh,
+                connected_services={"connectedService": {"status": "ACTIVE"}},
+            )
+
+            assert vehicle.vin == veh.get("vin")
+            assert vehicle.is_connected is True
+            print(vehicle.id)
+
+            if vehicle.vin is None:
+                assert vehicle.odometer is None
+                assert vehicle.energy is None
+                assert vehicle.hvac is None
+                assert vehicle.parking is None
+            else:
+                assert vehicle.odometer is not None
+                assert vehicle.energy is None
+                assert vehicle.hvac is None
+                assert vehicle.parking is not None
