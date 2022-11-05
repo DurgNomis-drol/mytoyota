b02ff0a7e649be30264be107efa40d972beb9bc1
Alexandar Mechev <dev@apmechev.com>
2022-11-05 15:09:33 +0100

    ✅ add tests for all lock responses

--- tests/test_lock_unlock.py
+++ tests/test_lock_unlock.py
@@ -15,7 +15,9 @@ from tests.test_myt import TestMyTHelper
 class TestLockUnlock(TestMyTHelper):
     """Pytest functions to test locking and unlocking"""
 
-    _lock_request_id = "d4f873d2-5da2-494f-a6d9-6e56d18d2ce9"
+    successful_lock_request_id = "d4f873d2-5da2-494f-a6d9-6e56d18d2ce9"
+    failed_lock_request_id = "14f873d2-5da2-494f-a6d9-6e56d18d2ce9"
+    pending_lock_request_id = "24f873d2-5da2-494f-a6d9-6e56d18d2ce9"
 
     def test_send_lock_request(self):
         """Test sending the lock request"""
@@ -26,11 +28,11 @@ class TestLockUnlock(TestMyTHelper):
         )
         assert isinstance(result, VehicleLockUnlockActionResponse)
         assert result.raw_json == {
-            "id": self._lock_request_id,
+            "id": self.pending_lock_request_id,
             "status": "inprogress",
             "type": "controlLock",
         }
-        assert result.request_id == self._lock_request_id
+        assert result.request_id == self.pending_lock_request_id
         assert result.status == "inprogress"
         assert result.type == "controlLock"
 
@@ -43,29 +45,56 @@ class TestLockUnlock(TestMyTHelper):
         )
         assert isinstance(result, VehicleLockUnlockActionResponse)
         assert result.raw_json == {
-            "id": self._lock_request_id,
+            "id": self.pending_lock_request_id,
             "status": "inprogress",
             "type": "controlLock",
         }
 
-    def test_get_lock_status(self):
+    def test_get_successful_lock_status(self):
         """Test getting the lock status"""
         myt = self._create_offline_myt()
         vehicle = self._lookup_vehicle(myt, 4444444)
         result = asyncio.get_event_loop().run_until_complete(
-            myt.get_lock_status(vehicle["vin"], self._lock_request_id)
+            myt.get_lock_status(vehicle["vin"], self.successful_lock_request_id)
         )
         assert isinstance(result, VehicleLockUnlockStatusResponse)
         assert result.raw_json == {
-            "id": self._lock_request_id,
+            "id": self.successful_lock_request_id,
             "status": "completed",
             "requestTimestamp": "2022-10-22T08:49:20.071Z",
             "type": "controlLock",
         }
-        assert result.request_id == self._lock_request_id
+        assert result.request_id == self.successful_lock_request_id
         assert result.status == "completed"
         assert result.type == "controlLock"
         assert result.request_timestamp == datetime(2022, 10, 22, 8, 49, 20, 71000)
+        assert not result.is_in_progress
+        assert not result.is_error
+        assert result.is_success
+
+    def test_get_failed_lock_status(self):
+        """Test getting the lock status"""
+        myt = self._create_offline_myt()
+        vehicle = self._lookup_vehicle(myt, 4444444)
+        result = asyncio.get_event_loop().run_until_complete(
+            myt.get_lock_status(vehicle["vin"], self.failed_lock_request_id)
+        )
+        assert isinstance(result, VehicleLockUnlockStatusResponse)
+        assert result.raw_json == {
+            "id": self.failed_lock_request_id,
+            "status": "error",
+            "errorCode": "LU0004",
+            "requestTimestamp": "2022-10-22T08:49:20.071Z",
+            "type": "controlLock",
+        }
+        assert result.request_id == self.failed_lock_request_id
+        assert result.status == "error"
+        assert result.error_code == "LU0004"
+        assert result.type == "controlLock"
+        assert result.request_timestamp == datetime(2022, 10, 22, 8, 49, 20, 71000)
+        assert not result.is_in_progress
+        assert result.is_error
+        assert not result.is_success
 
     def test_set_lock_vehicle_unsupported(self):
         """Test sending the lock request to a vehicle for which it is not supported"""
