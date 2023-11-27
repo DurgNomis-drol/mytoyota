1487d7eb29879a6ded16aadfe45a684f58191056
Simon Hörrle <simon.hoerrle@dm.de>
2023-11-27 09:03:20 +0100

    fix type hint

--- mytoyota/utils/logs.py
+++ mytoyota/utils/logs.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 
 def censor_value(value: Any, key: str, to_censor: set) -> Any:
@@ -13,7 +13,7 @@ def censor_value(value: Any, key: str, to_censor: set) -> Any:
     return value
 
 
-def censor_all(dictionary: dict[str, Any], to_censor: set = None) -> dict[str, Any]:
+def censor_all(dictionary: dict[str, Any], to_censor: Optional[set] = None) -> dict[str, Any]:
     if to_censor is None:
         to_censor = {
             "vin",
