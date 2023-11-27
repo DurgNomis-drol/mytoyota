7e7384c81b3c4c016a4c2dcda1dcf43618832fb7
Simon Hörrle <simon.hoerrle@dm.de>
2023-11-27 12:10:23 +0100

    cencor vin in HealtStatusModel

--- mytoyota/models/endpoints/healt_status.py
+++ mytoyota/models/endpoints/healt_status.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from typing import Any, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from mytoyota.utils.logs import censor_vin
 
 
 class HealtStatusModel(BaseModel):
@@ -9,3 +11,8 @@ class HealtStatusModel(BaseModel):
     vin: str
     warning: Optional[Any] = None
     wnglastUpdTime: datetime
+
+    @field_validator("vin")
+    def censor_original_vin(cls, v):
+        v = censor_vin(v)
+        return v
