b664326a693126bcc4c5c83159bd01c5e9b8d118
Simon Hörrle <simon.hoerrle@dm.de>
2023-12-01 16:58:17 +0100

    mark old tests as legacy and add some new tests for utils

--- mytoyota/utils/conversions.py
+++ mytoyota/utils/conversions.py
@@ -35,6 +35,4 @@ def convert_to_liter_per_100_miles(liters: float) -> float:
 def convert_to_mpg(liters_per_100_km: float) -> float:
     """Convert to miles per UK gallon (MPG)."""
     _LOGGER.debug("Converting to MPG...")
-    if liters_per_100_km > 0.0:
-        return round(282.5 / liters_per_100_km, 4)
-    return 0.0
+    return round(282.5 / liters_per_100_km, 4) if liters_per_100_km > 0.0 else 0.0
