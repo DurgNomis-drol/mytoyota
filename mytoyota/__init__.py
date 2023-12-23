5ad8bb91b487ee2f88a5b37d399230c075b56819
Simon Hörrle <simon.hoerrle@gmail.com>
2023-12-23 22:49:59 +0100

    adjust getting version info

--- mytoyota/__init__.py
+++ mytoyota/__init__.py
@@ -1,6 +1,6 @@
 """Toyota Connected Services Client."""
-import importlib.metadata as importlib_metadata
+from importlib.metadata import version
 
 from .client import MyT  # pylint: disable=unused-import # NOQA
 
-__version__ = importlib_metadata.version(__name__)
+__version__ = version(__name__)
