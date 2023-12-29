d04b8df3ef0c26ae7e2114c08fac219d26c03123
Simon Hörrle <simon.hoerrle@gmail.com>
2023-12-29 21:52:16 +0100

    use importlib_metadata as a dependency across all Python versions

--- mytoyota/__init__.py
+++ mytoyota/__init__.py
@@ -1,10 +1,6 @@
 """Toyota Connected Services Client."""
-from .client import MyT  # pylint: disable=unused-import # NOQA
+from importlib_metadata import version
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    # 3.7
-    import importlib_metadata
+from mytoyota.client import MyT  # pylint: disable=unused-import # NOQA
 
-__version__ = importlib_metadata.version(__name__)
+__version__ = version(__name__)
