5bc0c72925454d39d9b97548d519eb81506f6f2b
Simon Hörrle <simon.hoerrle@gmail.com>
2024-01-19 21:23:32 +0100

    Add service_history vehicle model

--- mytoyota/models/endpoints/service_history.py
+++ mytoyota/models/endpoints/service_history.py
@@ -1,5 +1,6 @@
 """Toyota Connected Services API - Service History Models."""
 
+from datetime import date
 from typing import Any, List, Optional
 
 from pydantic import BaseModel, Field
@@ -11,12 +12,12 @@ class ServiceHistoryModel(BaseModel):
     Attributes
     ----------
         customer_created_record (bool): Indicates if the record was created by the customer.
-        mileage (Optional[str]): The mileage at the time of the service.
+        mileage (Optional[int]): The mileage at the time of the service.
         notes (Any): Additional notes about the service.
         operations_performed (Any): The operations performed during the service.
         ro_number (Any): The RO (Repair Order) number associated with the service.
         service_category (str): The category of the service.
-        service_date (str): The date of the service.
+        service_date (date): The date of the service.
         service_history_id (str): The ID of the service history record.
         service_provider (str): The service provider.
         servicing_dealer (Any): The dealer that performed the service.
@@ -25,16 +26,16 @@ class ServiceHistoryModel(BaseModel):
     """
 
     customer_created_record: bool = Field(alias="customerCreatedRecord")
-    mileage: Optional[str]
+    mileage: Optional[int] = None
     notes: Any
     operations_performed: Any = Field(alias="operationsPerformed")
     ro_number: Any = Field(alias="roNumber")
     service_category: str = Field(alias="customerCreatedRecord")
-    service_date: str = Field(alias="serviceCategory")
+    service_date: date = Field(alias="serviceCategory")
     service_history_id: str = Field(alias="serviceHistoryId")
     service_provider: str = Field(alias="serviceProvider")
     servicing_dealer: Any = Field(alias="servicingDealer")
-    unit: Optional[str]
+    unit: Optional[str] = None
 
 
 class ServiceHistoriesModel(BaseModel):
