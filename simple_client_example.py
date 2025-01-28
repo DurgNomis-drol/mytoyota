6e06d1086e1548632f0cb351a956fd3ec8cad218
Simon Hörrle <simon.hoerrle@dm.de>
2025-01-28 10:54:01 +0100

    optimize simple_client_example

--- simple_client_example.py
+++ simple_client_example.py
@@ -29,7 +29,9 @@ def load_credentials():
 credentials = load_credentials()
 if not credentials:
     raise ValueError(
-        "Did you forget to set your username and password? Or supply the credentials file"
+        "Did you forget to set your username and password? Or supply the credentials file?"
+        "Please set your username and password in a file on top level called 'credentials.json' in the format:"  # noqa: E501
+        "{'username': '<username>', 'password': '<password>'}"
     )
 
 USERNAME = credentials["username"]
@@ -46,40 +48,25 @@ async def get_information():
     print("Retrieving cars...")
     cars = await client.get_vehicles(metric=True)
 
-    for car in cars:
-        await car.update()
-
-        # Dashboard Information
-        pp.pprint(f"Dashboard: {car.dashboard}")
-        # Location Information
-        pp.pprint(f"Location: {car.location}")
-        # Lock Status
-        pp.pprint(f"Lock Status: {car.lock_status}")
-        # Notifications
-        pp.pprint(f"Notifications: {[[x] for x in car.notifications]}")
-        # Service history
-        pp.pprint(f"Latest service: {car.get_latest_service_history()}")
-        # Summary
-        # pp.pprint(
-        #    f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=7), date.today(), summary_type=SummaryType.DAILY)]}"  # noqa: E501 # pylint: disable=C0301
-        # )
-        # pp.pprint(
-        #    f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=7 * 4), date.today(), summary_type=SummaryType.WEEKLY)]}"  # noqa: E501 # pylint: disable=C0301
-        # )
-        pp.pprint(
-            f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=6 * 30), date.today(), summary_type=SummaryType.MONTHLY)]}"  # noqa: E501
-        )
-        # pp.pprint(
-        #    f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=365), date.today(), summary_type=SummaryType.YEARLY)]}"  # noqa: E501 # pylint: disable=C0301
-        # )
-
-        # Trips
-        # pp.pprint(
-        #    f"Trips: f{await car.get_trips(date.today() - timedelta(days=7), date.today(), full_route=True)}"  # noqa: E501
-        # )
-
-        # Dump all the information collected so far:
-        # pp.pprint(car._dump_all())
+    if cars is not None:
+        for car in cars:
+            await car.update()
+            # Dashboard Information
+            pp.pprint(f"Dashboard: {car.dashboard}")
+            # Location Information
+            pp.pprint(f"Location: {car.location}")
+            # Lock Status
+            pp.pprint(f"Lock Status: {car.lock_status}")
+            # Notifications
+            pp.pprint(f"Notifications: {[[x] for x in car.notifications]}")
+            # Service history
+            pp.pprint(f"Latest service: {car.get_latest_service_history()}")
+            # Summary
+            pp.pprint(
+                f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=6 * 30), date.today(), summary_type=SummaryType.MONTHLY)]}"  # noqa: E501
+            )
+    if cars is None:
+        pp.pprint("No information on vehicles could be retrieved")
 
 
 loop = asyncio.get_event_loop()
