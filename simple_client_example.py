52468220d07a7a6fbabd73484b62e45afe17e45b
Simon Hörrle <simon.hoerrle@dm.de>
2025-01-29 08:44:47 +0100

    Implement simplificatio nhint from fabianfalon

--- simple_client_example.py
+++ simple_client_example.py
@@ -8,36 +8,24 @@ from mytoyota.client import MyT
 from mytoyota.models.summary import SummaryType
 
 pp = pprint.PrettyPrinter(indent=4)
-
-
-# Set your username and password in a file on top level called "credentials.json" in the format:
-#   {
-#       "username": "<username>",
-#       "password": "<password>"
-#   }
+CREDENTIALS_FILE_NAME = "credentials.json"
 
 
 def load_credentials():
-    """Load credentials from 'credentials.json'."""
+    """Load credentials from 'CREDENTIAL_FILE_NAME'."""
     try:
-        with open("credentials.json", encoding="utf-8") as f:
+        with open(CREDENTIALS_FILE_NAME, encoding="utf-8") as f:
             return json.load(f)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        return None
+    except (FileNotFoundError, json.decoder.JSONDecodeError, ValueError) as exc:
+        raise ValueError(
+            "Did you forget to set your username and password? Or supply the credentials file?"
+            f"Please set your username and password in a file on top level called '{CREDENTIALS_FILE_NAME}' in the format:"  # noqa: E501
+            "{'username': '<username>', 'password': '<password>'}"
+        ) from exc
 
 
 credentials = load_credentials()
-if not credentials:
-    raise ValueError(
-        "Did you forget to set your username and password? Or supply the credentials file?"
-        "Please set your username and password in a file on top level called 'credentials.json' in the format:"  # noqa: E501
-        "{'username': '<username>', 'password': '<password>'}"
-    )
-
-USERNAME = credentials["username"]
-PASSWORD = credentials["password"]
-
-client = MyT(username=USERNAME, password=PASSWORD)
+client = MyT(username=credentials["username"], password=credentials["password"])
 
 
 async def get_information():
@@ -46,29 +34,25 @@ async def get_information():
     await client.login()
 
     print("Retrieving cars...")
-    cars = await client.get_vehicles(metric=True)
-
-    if cars is not None:
-        for car in cars:
-            await car.update()
-            # Dashboard Information
-            pp.pprint(f"Dashboard: {car.dashboard}")
-            # Electric Status Information
-            pp.pprint(f"Electric Status: {car.electric_status}")
-            # Location Information
-            pp.pprint(f"Location: {car.location}")
-            # Lock Status
-            pp.pprint(f"Lock Status: {car.lock_status}")
-            # Notifications
-            pp.pprint(f"Notifications: {[[x] for x in car.notifications]}")
-            # Service history
-            pp.pprint(f"Latest service: {car.get_latest_service_history()}")
-            # Summary
-            pp.pprint(
-                f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=6 * 30), date.today(), summary_type=SummaryType.MONTHLY)]}"  # noqa: E501
-            )
-    if cars is None:
-        pp.pprint("No information on vehicles could be retrieved")
+    cars = await client.get_vehicles(metric=True) or []
+    for car in cars:
+        await car.update()
+        # Dashboard Information
+        pp.pprint(f"Dashboard: {car.dashboard}")
+        # Electric Status Information
+        pp.pprint(f"Electric Status: {car.electric_status}")
+        # Location Information
+        pp.pprint(f"Location: {car.location}")
+        # Lock Status
+        pp.pprint(f"Lock Status: {car.lock_status}")
+        # Notifications
+        pp.pprint(f"Notifications: {[[x] for x in car.notifications or []]}")
+        # Service history
+        pp.pprint(f"Latest service: {car.get_latest_service_history()}")
+        # Summary
+        pp.pprint(
+            f"Summary: {[[x] for x in await car.get_summary(date.today() - timedelta(days=6 * 30), date.today(), summary_type=SummaryType.MONTHLY)]}"  # noqa: E501
+        )
 
 
 loop = asyncio.get_event_loop()
